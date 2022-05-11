670512bca61b8a5dd2ed46b2c92d6ca7a3a0eab9
Sergio Andres Hernandez <sergsdev20@gmail.com>
2022-05-11 16:49:32 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: 'base', // only generate global styles
+      strategy: 'class', // only generate classes
+    }),
+  ],
+}
